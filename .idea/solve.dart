ae8f8f3ded51e606f58fcf621ab40bc5487e8b47
Shahlaa <zhussain2800@gmail.com>
2022-07-09 18:01:35 +0300

    Initial commit

---
+++ .idea/solve.dart
@@ -0,0 +1,26 @@
+void splitEvenOdd(){
+  //This part dentify the variables
+  List num = [22, 8, 7, 0, 13, 99, 38, 63];
+  List oddList = [];
+  List evenList = [];
+
+  //This part specific Check The Number in List is EvenNumber or oddNumber
+  and check The Number in List is oddNumber put in oddList */
+  for(final i in num){
+    if(i % 2 == 0)
+      evenList.add(i);
+    else
+      oddList.add(i);
+  }
+
+  //This part spacific print result
+  print('The List Even and odd Number: $num');
+  print('The Even lists: $evenList');
+  print('The odd list: $oddList');
+}
+
+main(){
+
+  //This part call the function
+  splitEvenOdd();
+}
