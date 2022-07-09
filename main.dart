14c2d10c026851c9956b5848c78a3545815ad0c9
Shahlaa <zhussain2800@gmail.com>
2022-07-09 18:19:31 +0300

    edit package

--- main.dart
+++ main.dart
@@ -5,7 +5,6 @@ void splitEvenOdd(){
   List evenList = [];
 
   //This part specific Check The Number in List is EvenNumber or oddNumber
-  and check The Number in List is oddNumber put in oddList */
   for(final i in num){
   if(i % 2 == 0)
   evenList.add(i);
